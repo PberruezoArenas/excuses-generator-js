fc264e5df33800b66ad13f7fda0c025a98b71159
PberruezoArenas <149729949+PberruezoArenas@users.noreply.github.com>
2023-11-17 17:15:18 +0000

    Version 1

--- src/app.js
+++ src/app.js
@@ -1,11 +1,32 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
+// import "bootstrap";
+import "./styles.css";
 
-import "./assets/img/rigo-baby.jpg";
+// import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+import "./assets/img/4K.png";
+// import ".src/file2.js";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  function generateExcuse() {
+    let who = ["The dog", "My grandma", "His turtle", "My bird"];
+    let action = ["ate", "peed", "crushed", "broke"];
+    let what = ["my homework", "the keys", "the car"];
+    let when = [
+      "before the class",
+      "right on time",
+      "when I finished",
+      "during my lunch",
+      "while I was praying"
+    ];
+
+    let randomWho = who[Math.floor(Math.random() * who.length)];
+    let randomAction = action[Math.floor(Math.random() * action.length)];
+    let randomWhat = what[Math.floor(Math.random() * what.length)];
+    let randomWhen = when[Math.floor(Math.random() * when.length)];
+
+    let excuse = `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}.`;
+
+    document.getElementById("excuseOutput").innerHTML = excuse;
+  }
 };
